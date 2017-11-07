c512b79802376c72d59a485aa121de272eb5b5b1
cmccormack <mccormack.christopher@gmail.com>
2017-11-06 21:20:40 -0700

    Added additional image viewer

--- app/api.js
+++ app/api.js
@@ -8,12 +8,13 @@ const search_options = {
 // Define example template values
 
 const templateOptions = {
-  hostname: 'https://imagus.glitch.me'
+  hostname: ''
 }
 
 module.exports = (app, db, externalAPI) => {
 
   app.get(['/','/imagesearch'], (req, res, next) => {
+    templateOptions.hostname = req.host
     res.render('examples', require('./exampleTemplate')(templateOptions))
   })
 
@@ -70,6 +71,35 @@ module.exports = (app, db, externalAPI) => {
     })
   })
 
+  // Call API just to view images (not a user story)
+  app.get('/:search', (req, res, next) => {
+    // Handle page offset if provided
+    if (req.query.offset && req.query.offset > 0) {
+      search_options.page = req.query.offset
+    }
+
+    // Call API with search parameters
+    externalAPI.search(req.params.search, search_options)
+      .then((results) => {
+        const imageTemplate = {
+          images: results.map(r => {
+            return { url: r.url, alt: r.alttext}
+          }),
+          title: 'Image Search Results',
+          search: req.params.search
+        }
+        res.render('images', imageTemplate)
+      })
+      .catch((err) => {
+        console.error(err)
+        res.type('json').send(
+          JSON.stringify({
+            query: req.params.search,
+            error: err.message
+          })
+        )
+      })
+  })
 
 }
 
